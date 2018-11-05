fada64f8600bb17531b9e9c6117a2e8f67f03dbe
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2018-11-05 23:42:28 +0900

    スライス

---
+++ 基礎編/要素の代入.py
@@ -0,0 +1,5 @@
+test_list = ['https', 'www', 'python', 'izm', 'com']
+ 
+test_list[1:3] = ('WWW', 'PYTHON')
+ 
+print(test_list)
---
+++ 基礎編/要素の取得5.py
@@ -0,0 +1,5 @@
+test_list = ['https', 'www', 'python', 'izm', 'com']
+ 
+print(test_list[-1:])   
+print(test_list[:-1])   
+print(test_list[::-1]) 
