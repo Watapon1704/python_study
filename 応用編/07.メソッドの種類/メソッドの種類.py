35c58d90c1bed5fa244b46ce1bdfbcd7ec599f1f
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2019-02-05 22:39:58 +0900

    メソッドの種類の成果物

---
+++ 応用編/07.メソッドの種類/メソッドの種類.py
@@ -0,0 +1,12 @@
+﻿class TestClass:
+ 
+    def sample_instancemethod(self, arg_1):
+        pass
+ 
+    @classmethod
+    def sample_classmethod(cls, arg_1):
+        pass
+ 
+    @staticmethod
+    def sample_staticmethod(arg_1, arg_2):
+        pass
