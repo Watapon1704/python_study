ca0161a58e6e1030d0d312c5fa09c45442145c46
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2019-02-05 22:20:34 +0900

    関数・メソッドの成果物

---
+++ 応用編/関数・メソッド/関数とメソッド.py
@@ -0,0 +1,21 @@
+﻿def test_func():
+    print('call test_func')
+  
+  
+class TestClass:
+
+    def test_method():
+        print('call test_method')
+ 
+print('------------------------')
+print(test_func)
+print(TestClass.test_method)
+ 
+print('------------------------')
+print(type(test_func))
+print(type(TestClass.test_method))
+ 
+print('------------------------')
+t = TestClass()
+print(test_func)
+print(t.test_method)
