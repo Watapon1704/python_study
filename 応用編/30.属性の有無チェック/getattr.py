af7beebad3bb7e68b4c506198daeae18a9c3b5e8
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2019-03-26 23:34:57 +0900

    応用編の成果物

---
+++ 応用編/30.属性の有無チェック/getattr.py
@@ -0,0 +1,13 @@
+﻿class AttrTest():
+ 
+    def __init__(self):
+        self.code = -1
+ 
+ 
+attr_test = AttrTest()
+attr_test.name = 'python-izm'
+ 
+print(getattr(attr_test, 'code'))
+print(getattr(attr_test, 'name'))
+# print(getattr(attr_test, 'kana'))
+print(getattr(attr_test, 'kana', 'No Attr'))
---
+++ 応用編/32.最小値・最大値の取得/key引数.py
@@ -0,0 +1,8 @@
+﻿def key_func(n):
+    return int(n)
+ 
+ 
+l = [2, 3, 4, '111']
+ 
+print(min(l, key=key_func))
+print(max(l, key=key_func))
