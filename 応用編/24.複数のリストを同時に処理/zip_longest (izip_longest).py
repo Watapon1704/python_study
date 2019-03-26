88cea9c35de75737e13c2bb9cf51febee0004639
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2019-03-26 23:11:43 +0900

    応用編の成果物

---
+++ 応用編/24.複数のリストを同時に処理/zip_longest (izip_longest).py
@@ -0,0 +1,5 @@
+﻿item_list = ['desktop', 'laptop', 'tablet', 'smartphone']
+stock_list = [12, 83, 55]
+ 
+for item_name, stock_count in zip(item_list, stock_list):
+    print('{} / {}'.format(item_name, stock_count))
---
+++ 応用編/27.セットの比較・作成・更新/セットを比較して作成.py
@@ -0,0 +1,6 @@
+﻿test_set_1 = {'python', 'izm', 'com'}
+ 
+print(test_set_1.union({'python', 'www'}))
+print(test_set_1.intersection({'python', 'www'}))
+print(test_set_1.difference({'python', 'www'}))
+print(test_set_1.symmetric_difference({'python', 'www'}))
