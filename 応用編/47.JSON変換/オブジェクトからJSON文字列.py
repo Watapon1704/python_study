a196c692ff5b232c108f301ce5e165bc781df55e
Watapon1704 <38561500+Watapon1704@users.noreply.github.com>
2019-03-27 21:48:44 +0900

    応用編の成果物

---
+++ 応用編/47.JSON変換/オブジェクトからJSON文字列.py
@@ -0,0 +1,11 @@
+﻿import json
+ 
+json_data = {'Python':'python-izm.com',
+             'SearchEngine':('google.co.jp', 'yahoo.co.jp')}
+ 
+print(type(json_data))
+ 
+encode_json_data = json.dumps(json_data)
+ 
+print(encode_json_data)
+print(type(encode_json_data))
